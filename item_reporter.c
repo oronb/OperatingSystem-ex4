5e27ef2bc8d7f44498a3266c5ef0fbec5ead66b4
oronBortman <oron.bortman@gmail.com>
2021-01-16 13:52:58 +0200

    Adding changes

---
+++ item_reporter.c
@@ -0,0 +1,19 @@
+#include "item_reporter.h"
+
+int main()
+{
+    int pip[2];
+    int ret;
+    struct item itemGiven;
+    //pipe(pip);
+    close(pip[1]);
+    while(1)
+    {
+        ret = read(pip[0], &itemGiven, sizeof(struct item));
+        if(ret == 0)
+        {
+            exit(0);
+        }
+        printf("@@@number 1:%d, number 2:%d\n\n",itemGiven.n1, itemGiven.n2);
+    }
+}
