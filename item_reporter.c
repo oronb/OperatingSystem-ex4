62bfce79c4074c880d73149abbf6e3ec5fec92f1
oronBortman <oron.bortman@gmail.com>
2021-01-16 15:43:23 +0200

    Adding changes

--- item_reporter.c
+++ item_reporter.c
@@ -2,20 +2,16 @@
 
 int main()
 {
-  //int pip[2];
     int ret;
     struct item itemGiven;
-    //pipe(pip);
-    //close(pip[1]);
     while(1)
     {
-        int fd = dup(0);
-        ret = read(fd, &itemGiven, sizeof(struct item));
+        ret = read(0, &itemGiven, sizeof(struct item));
         if(ret == 0)
         {
             exit(0);
         }
-        printf("@@@prod%d\n\n",itemGiven.prod);
-        //sleep(10);
+        printf(REPORTER_MSG , "  ");
+        print_one_item(&itemGiven);
     }
 }
